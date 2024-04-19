13022e1b12fb3f588acbafeff1d099d31ed0a74d
Zel <zelgehai@gmail.com>
2024-04-18 20:14:11 -0700

    Default Created Projects

---
+++ Debug/main.d
@@ -0,0 +1,21 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /Applications/ti/ccs1260/ccs/ccs_base/msp430/include/msp430.h
+main.obj: /Applications/ti/ccs1260/ccs/ccs_base/msp430/include/cc430f5123.h
+main.obj: /Applications/ti/ccs1260/ccs/ccs_base/msp430/include/in430.h
+main.obj: /Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: /Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+
+../main.c:
+
+/Applications/ti/ccs1260/ccs/ccs_base/msp430/include/msp430.h:
+
+/Applications/ti/ccs1260/ccs/ccs_base/msp430/include/cc430f5123.h:
+
+/Applications/ti/ccs1260/ccs/ccs_base/msp430/include/in430.h:
+
+/Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+/Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
